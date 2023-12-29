981b0833a2d9c2cb7f1594b0d186a2d0ded85556
Godruoyi <godruoyi@gmail.com>
2023-12-29 19:44:32 +0800

    chore: remove type

--- src/types.ts
+++ src/types.ts
@@ -48,38 +48,3 @@ export interface ISettings {
 export interface IError {
     message: string
 }
-
-export interface IStorage {
-    store(link: ILink): Promise<IError | void>
-}
-
-export interface IProvider {
-    /**
-     * Boot the provider when all providers are registered.
-     *
-     * @throws Error boot error, such as cannot connect to service, etc.
-     */
-    boot(): Promise<void>
-
-    /**
-     * Get the provider name.
-     *
-     * We will use this name to get the provider config that from the browser storage.
-     */
-    name(): string
-
-    /**
-     * Register a service provider.
-     *
-     * @param config provider config
-     * @throws Error registration service provider error, such as invalid config, cannot connect to service, etc.
-     */
-    register(config: IConfig): Promise<void>
-
-    /**
-     * Provider a storage service.
-     *
-     * Should always return a valid storage service and never throw an error.
-     */
-    provider(): IStorage
-}
